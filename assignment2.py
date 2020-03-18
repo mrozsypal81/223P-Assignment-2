a87af3875dedcd7ce43f964226f493a3ab549600
cpsc455 <wolfken81@gmail.com>
2020-03-18 15:53:13 -0700

    finished the first problem and starting working on the 2nd

--- assignment2.py
+++ assignment2.py
@@ -1,12 +1,31 @@
 #Assignment 2 by Michael Rozsypal
 #Problem 1 Python Lambda Function
 
+import itertools
+import functools
+
 
 decisionvalue = None
 
 
-def calc_poly_func(r,n,m):
+def lambdafunc(r,n,m):
+
     return lambda a,x,y : (a/r)*(x**n)*(y**m)
+
+def cal_poly_func(r,n,m):
+    outputl = []
+    newlam = lambdafunc(r,n,m)
+    for (i,j,k) in zip(a,x,y):
+        outputl.append(newlam(i,j,k))
+    return outputl
+
+def combine_with_list(list_obj,list2_obj):
+    print("doing combine lists ")
+
+def combine_with_set(list_obj,set_obj):
+    print("doing combine set with list ")
+
+    
 
 
 while decisionvalue != 4 :
@@ -28,7 +47,22 @@ while decisionvalue != 4 :
     
     if decisionvalue == "1":
         print("Starting Problem 1")
-        calc_poly_func(a,x,y)
+        r,n,m = input("Enter three values with spaces inbetween for constants ").split()
+
+        r = int(r,10)
+        n = int(n,10)
+        m = int(m,10)
+
+        a = list(map(int,input("Enter values for the first list a: ").split()))
+        x = list(map(int,input("Enter values for the second list x: ").split()))
+        y = list(map(int,input("Enter values for the third list y: ").split()))
+
+        outputl = cal_poly_func(r,n,m)
+
+        outputsum = functools.reduce(lambda a,b : a + b,outputl)
+
+        print("The sum of the lists together modified by the constants is ", outputsum)
+
     elif decisionvalue == "2":
         print("Starting Problem 2")
     elif decisionvalue == "3":
